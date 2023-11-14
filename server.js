983f0aacbdc3522bdac7ba07a9c3f5d696f6ae5b
Maxfield Kraus <maxfield.kraus@gmail.com>
2023-11-13 21:42:28 -0500

    Added styling

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-//app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
 
